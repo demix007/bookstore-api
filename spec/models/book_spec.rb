8dae9d3a4436bb5239fbce3336e449c2779924cf
demix007 <ayodejiabidemi007@gmail.com>
2023-05-05 13:14:37 +0100

    ADD LINTERS AND ADD RSPEC TESTS

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association tests
+
+  it { should belong_to(:Category) }
+
+  # Validation tests
+
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:author) }
+  it { should validate_length_of(:title).is_at_least(3) }
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association tests
+  it { should have_many(:books) }
+
+  # Validation tests
+
+  it { should validate_presence_of(:name) }
+  it { should validate_length_of(:name).is_at_least(3) }
 end
