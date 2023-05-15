628190c17e33bc2393095d9c38960ef08aaf5501
demix007 <ayodejiabidemi007@gmail.com>
2023-05-15 19:32:59 +0100

    add CategoriesRepresenterto render JSON response

---
+++ app/representers/categories_representer.rb
@@ -0,0 +1,15 @@
+class CategoriesRepresenter
+    def initialize(categories)
+      @categories = categories
+    end
+    def as_json
+      categories.map do |category|
+        {
+          id: category.id,
+          name: category.name
+        }
+      end
+    end
+    private
+    attr_reader :categories
+end
