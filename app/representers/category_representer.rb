dc1dfa99abf51dbebda9f04fc38ebe41e97442f4
demix007 <ayodejiabidemi007@gmail.com>
2023-05-15 19:32:25 +0100

    add CategoryRepresenter to render JSON response

---
+++ app/representers/category_representer.rb
@@ -0,0 +1,13 @@
+class CategoryRepresenter
+    def initialize(category)
+      @category = category
+    end
+    def as_json
+      {
+        id: category.id,
+        name: category.name
+      }
+    end
+    private
+    attr_reader :category
+   end
